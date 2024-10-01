cc4f13b9f33ff2bedfbcfac3ca31c18c1822f23b
KimGeonWoo <ikkw2040@naver.com>
2024-10-01 16:56:52 +0900

    init

---
+++ src/main/java/com/baseballapp/playbaseball/BaseballappApplication.java
@@ -0,0 +1,13 @@
+package com.baseballapp.playbaseball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseballappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baseballapp/playbaseball/BaseballappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baseballapp.playbaseball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseballappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
