3c35dbfce045d7c8499838ec7c84b1e7612658fa
KimGeonWoo <ikkw2040@naver.com>
2024-10-03 13:08:48 +0900

    모듈 분리

--- bba.playbaseball/src/test/java/com/playbaseball/playbaseball/ApplicationTests.java
+++ bba.playbaseball/src/test/java/com/playbaseball/playbaseball/ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.baseballapp.playbaseball;
+package com.playbaseball.playbaseball;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BaseballappApplicationTests {
+class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
